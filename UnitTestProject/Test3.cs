27594123f34ca04c78c96f33b35811034fb99e4f
Bartosz Dziuba <01163817@pw.edu.pl>
2020-11-20 17:54:08 +0100

    Test3 Class Added

---
+++ UnitTestProject/Test3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    [TestCategory("Part3")]
+    public class Test3
+    {
+        private int a, b;
+
+        [TestInitialize]
+        public void RunBeforeEveryTest()
+        {
+            a = 1;
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            b = 2;
+            Assert.AreEqual(3, a + b);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.IsTrue(a == 1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(AssertFailedException))]
+        public void TestMethod3()
+        {
+            Assert.AreNotEqual(a, 1);
+        }
+    }
+}
