b689dfc1bbcdf92bf1c689eec1888238565485a2
Bartosz Dziuba <01163817@pw.edu.pl>
2020-11-20 21:23:08 +0100

    Car class added

---
+++ Section6/Car.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section6
+{
+    class Car
+    {
+        public string color;
+        public int numberOfDoors;
+        public bool isConvertable;
+
+        public string Color
+        {
+            get
+            { return color; }
+            set
+            { color = value; }
+        }
+
+        public int NumberOfDoors
+        {
+            get { return numberOfDoors; }
+            set
+            {
+                if (value <= 5)
+                {
+                    numberOfDoors = value;
+                }
+                else
+                {
+                    Console.WriteLine("Get real. Less than 6 doors.");
+                }
+            }
+        }
+
+        public bool IsConvertable { get; set; }
+
+        public Car(string carColor, int doors, bool convertable)
+        {
+            Color = carColor;
+            NumberOfDoors = doors;
+            IsConvertable = convertable;
+        }
+    }
+}
