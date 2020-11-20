2d14dabace4a88e31da7649970ba290a5126aa20
Bartosz Dziuba <01163817@pw.edu.pl>
2020-11-20 17:20:35 +0100

    Start

--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -4,11 +4,17 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace UnitTestProject
 {
     [TestClass]
+    [TestCategory("LoginFeature")]
     public class UnitTest1
     {
         [TestMethod]
+        [TestCategory("Positive")]
+        [Description("these are demo unit tests")]
         public void TestMethod1()
         {
+            var a = 1;
+            var b = 2;
+            Assert.AreEqual(3, a + b);
         }
     }
 }
