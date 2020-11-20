3d4a129f97c72bbab14e73a4930c46f66b342bc3
Bartosz Dziuba <01163817@pw.edu.pl>
2020-11-20 23:08:49 +0100

    Section Exam - Trip test and class

---
+++ Section6/Trip.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section6
+{
+    class Trip
+    {
+        //Constructors
+        public Trip(string dest, int distance, double cost, double fuel)
+        {
+            Destination = dest;
+            DistanceTravelled = distance;
+            TotalCostOfFuel = cost;
+            FuelConsumed = fuel;
+        }
+
+        public Trip(int distance, double cost, double fuel)
+        {
+            Destination = "Wherever";
+            DistanceTravelled = distance;
+            TotalCostOfFuel = cost;
+            FuelConsumed = fuel;
+        }
+
+        //Properties
+        public string Destination { get; set; }
+        public int DistanceTravelled { get; set; }
+        public double TotalCostOfFuel { get; set; }
+        public double FuelConsumed { get; set; }
+
+        //Methods
+        public double MilesPerGallon()
+        {
+            return DistanceTravelled / FuelConsumed;
+        }
+
+        public double CostPerMile()
+        {
+            return TotalCostOfFuel / DistanceTravelled;
+        }
+
+        public override string ToString()
+        {
+            return $"Trip to {Destination}: {DistanceTravelled} miles. Money spent on fuel: ${TotalCostOfFuel}. Fuel consumed: {FuelConsumed} gallons.";
+        }
+    }
+}
---
+++ Section6/TripTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Section6
+{
+    [TestClass]
+    public class TripTest
+    {
+        [TestMethod]
+        public void Trip_Test_1()
+        {
+            Trip vancouverTrip = new Trip("Vancouver", 2345, 200, 95);
+
+            Console.WriteLine(vancouverTrip);
+            Console.WriteLine($"Miles Per Gallon: {vancouverTrip.MilesPerGallon()}");
+            Assert.AreEqual(24.68, vancouverTrip.MilesPerGallon(), 0.01);
+        }
+
+        [TestMethod]
+        public void Trip_Test_2()
+        {
+            Trip vancouverTrip = new Trip("Vancouver", 2345, 200, 95);
+
+            Console.WriteLine(vancouverTrip);
+            Console.WriteLine($"Cost Per Mile: {vancouverTrip.CostPerMile()}");
+            Assert.AreEqual(0.085, vancouverTrip.CostPerMile(), 0.001);
+        }
+    }
+}
