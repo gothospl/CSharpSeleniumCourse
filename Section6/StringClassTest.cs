16809e3121b8426dd874e397d74b22aace67b0b8
Bartosz Dziuba <01163817@pw.edu.pl>
2020-11-20 22:06:23 +0100

    String class section

---
+++ Section6/StringClassTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Section6
+{
+    [TestClass]
+    public class StringClassTest
+    {
+        [TestMethod]
+        public void Using_String_Class()
+        {
+            string str = "Moja poezja to jest... wielkie uspokojenie";
+            Console.WriteLine(str);
+            string substr = str.Substring(12);
+            Console.WriteLine(substr);
+        }
+
+        [TestMethod]
+        public void Joining_Strings_Together()
+        {
+            char[] chars = { 'K', 'o', 'l', 'o', 'n', 'i', 'i' };
+            string name = new string(chars);
+            Console.WriteLine($"Witamy w {name}");
+        }
+    }
+}
