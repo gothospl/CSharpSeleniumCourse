9c9c709411be9bba686081fda6eb348df1d72a75
Bartosz Dziuba <01163817@pw.edu.pl>
2020-11-20 20:00:27 +0100

    Section 5 quiz

---
+++ Section5/SectionQuiz.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Section5
+{
+    [TestClass]
+    public class SectionQuiz
+    {
+        static double givenF, givenC;
+
+        [ClassInitialize]
+        public static void TestSetupClass(TestContext testContext)
+        {
+            givenF = 23;
+            givenC = 10;
+        }
+
+        [TestMethod]
+        public void Convert_F_To_C()
+        {
+            double resultC = (givenF - 32.0) / 1.8;
+            Console.WriteLine($"The temp in F {givenF} is {resultC} in C.");
+            Assert.AreEqual(-5.0, resultC);
+        }
+
+        [TestMethod]
+        public void Convert_C_To_F()
+        {
+            double resultF = givenC * 1.8 + 32.0;
+            Console.WriteLine($"The temp in C {givenC} is {resultF} in F.");
+            Assert.AreEqual(50.0, resultF);
+        }
+    }
+}
