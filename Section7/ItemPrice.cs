e42efef529158551881b3fb5688e17fd0510f08b
Bartosz Dziuba <01163817@pw.edu.pl>
2020-11-23 14:45:50 +0100

    Resale Value quiz complete

---
+++ Section7/ItemPrice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section7
+{
+    class ItemPrice
+    {
+        public decimal WholesaleItemPrice { get; set; }
+
+        public ItemPrice(decimal wholesaleItemPrice)
+        {
+            WholesaleItemPrice = wholesaleItemPrice;
+        }
+    }
+}
---
+++ Section7/ItemPriceTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Section7
+{
+    [TestClass]
+    public class ItemPriceTest
+    {
+        [TestMethod]
+        public void Test_Markup_Table()
+        {
+            ItemPrice TestPrice = new ItemPrice(34.12M);
+            Console.WriteLine($"Markup:\tNewPrice:");
+
+            for (int markupValue = 5; markupValue <= 10; markupValue++)
+            {
+                decimal newItemPrice = TestPrice.WholesaleItemPrice + (markupValue*TestPrice.WholesaleItemPrice)/100;
+                Console.WriteLine($"{markupValue}%\t{newItemPrice.ToString("c")}");
+            }
+        }
+    }
+}
