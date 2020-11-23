22e2a99d995936b995be630a4ba17496acae08f8
Bartosz Dziuba <01163817@pw.edu.pl>
2020-11-23 16:39:58 +0100

    Methods quiz complete

---
+++ Section8/MathFunctions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section8
+{
+    class MathFunctions
+    {
+        public static double SquareRoot(double aValue)
+        {
+            return Math.Sqrt(aValue);
+        }
+
+        public static int Sum(int aValue, int bValue)
+        {
+            return aValue + bValue;
+        }
+
+        public static double Sum(double aValue, double bValue)
+        {
+            return aValue + bValue;
+        }
+
+        public static decimal Sum(decimal aValue, decimal bValue)
+        {
+            return aValue + bValue;
+        }
+    }
+}
---
+++ Section8/MathFunctionsTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Section8
+{
+    [TestClass]
+    public class MathFunctionsTest
+    {
+        [TestMethod]
+        public void Test_Square_Root()
+        {
+            double expectedResult = 4.0;
+            double input = 16.0;
+            double actualResult = MathFunctions.SquareRoot(input);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Test_Sum_Int()
+        {
+            int expectedResult = 35;
+            int input1 = 12;
+            int input2 = 23;
+            int actualResult = MathFunctions.Sum(input1, input2);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Test_Sum_Double()
+        {
+            double expectedResult = 66.0;
+            double input1 = 20.13;
+            double input2 = 45.87;
+            double actualResult = MathFunctions.Sum(input1, input2);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Test_Sum_Decimal()
+        {
+            decimal expectedResult = 13.45M;
+            decimal input1 = 6.213M;
+            decimal input2 = 7.237M;
+            decimal actualResult = MathFunctions.Sum(input1, input2);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
