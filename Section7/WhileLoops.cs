cecdbb5bd22316c7be5e4f9ab3a3d0be67199372
Bartosz Dziuba <01163817@pw.edu.pl>
2020-11-23 13:56:16 +0100

    More while loops - sentinel, nested

--- Section7/WhileLoops.cs
+++ Section7/WhileLoops.cs
@@ -16,5 +16,35 @@ namespace Section7
                 number++;
             }
         }
+
+        [TestMethod]
+        public void Test_Sentinel_Controlled_Loop()
+        {
+            string inValue = "";
+            while (inValue != "-99")
+            {
+                Console.Write("\nEnter value (-99 to exit): ");
+                inValue = Console.ReadLine();
+            }
+            Console.ReadKey();
+        }
+
+        [TestMethod]
+        public void Nested_Loops()
+        {
+            int number1 = 1;
+            int number2 = 1;
+
+            while(number1 <= 5)
+            {
+                while(number2 <= number1)
+                {
+                    Console.WriteLine(number2);
+                    number2++;
+                }
+                number1++;
+            }
+        }
     }
 }
+;
