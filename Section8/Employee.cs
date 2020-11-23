4ef46a11c51df813bea6bb96ebf88d6b9ae9ff25
Bartosz Dziuba <01163817@pw.edu.pl>
2020-11-23 16:18:24 +0100

    Method overloading

---
+++ Section8/Employee.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section8
+{
+    class Employee
+    {
+        //Constructors
+        public Employee(int id)
+        {
+            EmployeeID = id;
+        }
+
+        public Employee(string name, int id, string title)
+        {
+            Name = name;
+            EmployeeID = id;
+            JobTitle = title;
+        }
+
+        //Properties
+        public int EmployeeID { get; set; }
+        public string Name { get; set; }
+        public string JobTitle { get; set; }
+
+        //Methods
+        public string JobLocation()
+        {
+            if (JobTitle == "Manager")
+            {
+                return "Boston";
+            }
+            else if (JobTitle == "Staff")
+            {
+                return "Chicago";
+            }
+
+            return "New York";
+        }
+
+        public string PayType(string title)
+        {
+            if (JobTitle == "Manager")
+                return "Salary";
+            else
+                return "Hourly";
+        }
+
+        public string PayType(int id)
+        {
+            if (EmployeeID == 12345)
+                return "Salary";
+            else
+                return "Hourly";
+        }
+
+        public override string ToString()
+        {
+            return $"{EmployeeID} {Name} Job Title: {JobTitle}";
+        }
+    }
+}
--- Section8/MethodTest.cs
+++ Section8/MethodTest.cs
@@ -12,5 +12,21 @@ namespace Section8
             int result = MathHelper.Square(6);
             Assert.AreEqual(36, result);
         }
+
+        [TestMethod]
+        public void Test_Employee_Overload_String()
+        {
+            Employee testEmployee = new Employee("John Curtin", 12345, "Manager");
+            string result = testEmployee.PayType(testEmployee.JobTitle);
+            Assert.AreEqual("Salary", result);
+        }
+
+        [TestMethod]
+        public void Test_Employee_Overload_Int()
+        {
+            Employee testEmployee = new Employee("John Curtin", 12345, "Manager");
+            string result = testEmployee.PayType(testEmployee.EmployeeID);
+            Assert.AreEqual("Salary", result);
+        }
     }
 }
