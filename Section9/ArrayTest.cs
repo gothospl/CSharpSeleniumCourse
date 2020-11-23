87574a22b72e468714abea215ba021c62ca89749
Bartosz Dziuba <01163817@pw.edu.pl>
2020-11-23 17:55:02 +0100

    Arrays intro, ForEach loop

---
+++ Section9/ArrayTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Section9
+{
+    [TestClass]
+    public class ArrayTest
+    {
+        [TestMethod]
+        public void Array_Test()
+        {
+            int[] scores = new int[14];
+            scores[0] = 10;
+            int score = scores[0];
+            Assert.AreEqual(10, score);
+
+            int numPositions = 10;
+            int[] testArray = new int[numPositions];
+        }
+
+        [TestMethod]
+        public void ForEach_Loop_Test()
+        {
+            int[] scores = { 2, 4, 6, 8, 10 };
+
+            foreach(int score in scores)
+            {
+                Console.WriteLine(score);
+            }
+        }
+    }
+}
---
+++ Section9/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Section9")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Section9")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("837fb2e4-72ee-4d8c-bbdd-b65348d16e29")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
