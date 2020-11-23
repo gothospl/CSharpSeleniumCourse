341d0938b2eaa572941b93a5bc8bfa8a6f87417b
Bartosz Dziuba <01163817@pw.edu.pl>
2020-11-23 16:06:20 +0100

    Method Call Stack intro

---
+++ Section8/MethodTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Section8
+{
+    [TestClass]
+    public class MethodTest
+    {
+        [TestMethod]
+        public void Test_Square()
+        {
+            MethodTest myTest = new MethodTest();
+            int result = myTest.Square(6);
+            Assert.AreEqual(36, result);
+        }
+
+        public int Square(int aValue)
+        {
+            return aValue * aValue;
+        }
+    }
+}
---
+++ Section8/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Section8")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Section8")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("31ac6ca2-b9fb-4c2e-8706-1ffd5bb1acba")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
