34a113d45d57654c7b8f2aeb36ce351de6c8765f
Bartosz Dziuba <01163817@pw.edu.pl>
2020-11-23 17:36:22 +0100

    Property Tax Calculator exam - attempt

---
+++ Section8/PropertyTaxCalculator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section8
+{
+    class PropertyTaxCalculator
+    {
+        private decimal currentYearAssessedValue;
+
+        public string PropertyAddress { get; set; }
+        public decimal LastYearAssessedValue { get; set; }
+        public decimal Exemption { get; set; }
+        public decimal MillageRate { get; set; }
+
+        public PropertyTaxCalculator(string propertyAddress, decimal lastYearAssessedValue)
+        {
+            PropertyAddress = propertyAddress;
+            LastYearAssessedValue = lastYearAssessedValue;
+            Exemption = 25000M;
+            MillageRate = 10.03M;
+        }
+
+        public decimal CurrentYearAssessedValue()
+        {
+            return LastYearAssessedValue * 1.027M;
+        }
+
+        public decimal TaxableValue()
+        {
+            return Decimal.Subtract(CurrentYearAssessedValue(), Exemption);
+        }
+
+        public decimal TaxesDue()
+        {
+            return TaxableValue() * MillageRate / 1000M;
+        }
+
+        public override string ToString()
+        {
+            return $"Property Address:\t{PropertyAddress}"+
+                $"\nLast Year Assessed Value:\t{LastYearAssessedValue.ToString("c")}"+
+                $"\nCurrent Assessed Value:\t{CurrentYearAssessedValue().ToString("c")}"+
+                $"\nExemption:\t{Exemption.ToString("c")}"+
+                $"\nTaxable Value:\t{TaxableValue().ToString("c")}"+
+                $"\nMillage Rate (per $1000):\t{MillageRate.ToString("c")}+" +
+                $"\nTaxes Due:\t{TaxesDue().ToString("c")}";
+        }
+
+    }
+}
---
+++ Section8/PropertyTaxTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Section8
+{
+    [TestClass]
+    public class PropertyTaxTest
+    {
+        [TestMethod]
+        public void Test_Taxable_Value()
+        {
+            PropertyTaxCalculator myProperty = new PropertyTaxCalculator("18 Paper Street, 18829 Wilmington, DE", 300000M);
+            Assert.AreEqual(283100M, myProperty.TaxableValue());
+        }
+
+        [TestMethod]
+        public void Test_Taxes_Due()
+        {
+            PropertyTaxCalculator myProperty = new PropertyTaxCalculator("18 Paper Street, 18829 Wilmington, DE", 300000M);
+            Assert.AreEqual(2839.493M, myProperty.TaxesDue());
+        }
+
+        [TestMethod]
+        public void Test_Three_Homes_And_Print()
+        {
+            PropertyTaxCalculator propertyOne = new PropertyTaxCalculator("18 Paper Street, 18829 Wilmington, DE", 300000M);
+            PropertyTaxCalculator propertyTwo = new PropertyTaxCalculator("12 Columbia Avenue, 90210 Beverly Hills, CA", 1700000M);
+            PropertyTaxCalculator propertyThree = new PropertyTaxCalculator("227 Browning Street, 12345 Denver, CO", 150000M);
+
+            Console.WriteLine(propertyOne);
+            Console.WriteLine(propertyTwo);
+            Console.WriteLine(propertyThree);
+        }
+    }
+}
