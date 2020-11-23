fbd82fd8200c480016ad13a32cf315c400782f95
Bartosz Dziuba <01163817@pw.edu.pl>
2020-11-23 13:50:02 +0100

    While loops start

---
+++ Section7/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Section7")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Section7")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b3870214-6651-4795-829d-6ccdbab2d66a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Section7/WhileLoops.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Section7
+{
+    [TestClass]
+    public class WhileLoops
+    {
+        [TestMethod]
+        public void Test_While_Loops()
+        {
+            int number = 1;
+            while(number < 11)
+            {
+                Console.WriteLine(number);
+                number++;
+            }
+        }
+    }
+}
