5759d381aa588fdf04051512e34197f4eceb536a
Bartosz Dziuba <01163817@pw.edu.pl>
2020-11-23 14:04:32 +0100

    Nested loops quiz

---
+++ Section7/NestedLoopsQuiz.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Section7
+{
+    [TestClass]
+    public class NestedLoopsQuiz
+    {
+        [TestMethod]
+        public void Nested_Loops_Quiz()
+        {
+            int i = 0;
+            int j = 10;
+            while (i <= 2)
+            {
+                while (j >= 6)
+                {
+                    Console.WriteLine($"Outer: {i}      Inner: {j}");
+                    j--;
+                }
+                i++;
+                j = 10;
+            }
+        }
+    }
+}
