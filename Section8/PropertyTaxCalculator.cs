f86dbb90e23eaf667c650e2351e8b832b8f5d18e
Bartosz Dziuba <01163817@pw.edu.pl>
2020-11-23 17:44:30 +0100

    Property Tax Calculator refactoring

--- Section8/PropertyTaxCalculator.cs
+++ Section8/PropertyTaxCalculator.cs
@@ -8,34 +8,32 @@ namespace Section8
 {
     class PropertyTaxCalculator
     {
-        private decimal currentYearAssessedValue;
+        private decimal millage_rate = 10.03M;
+        private decimal assessment_increase = 0.027M;
+        private decimal exemption = 25000M;
 
         public string PropertyAddress { get; set; }
         public decimal LastYearAssessedValue { get; set; }
-        public decimal Exemption { get; set; }
-        public decimal MillageRate { get; set; }
 
         public PropertyTaxCalculator(string propertyAddress, decimal lastYearAssessedValue)
         {
             PropertyAddress = propertyAddress;
             LastYearAssessedValue = lastYearAssessedValue;
-            Exemption = 25000M;
-            MillageRate = 10.03M;
         }
 
         public decimal CurrentYearAssessedValue()
         {
-            return LastYearAssessedValue * 1.027M;
+            return LastYearAssessedValue * (1M + assessment_increase);
         }
 
         public decimal TaxableValue()
         {
-            return Decimal.Subtract(CurrentYearAssessedValue(), Exemption);
+            return Decimal.Subtract(CurrentYearAssessedValue(), exemption);
         }
 
         public decimal TaxesDue()
         {
-            return TaxableValue() * MillageRate / 1000M;
+            return TaxableValue() * millage_rate / 1000M;
         }
 
         public override string ToString()
@@ -43,9 +41,9 @@ namespace Section8
             return $"Property Address:\t{PropertyAddress}"+
                 $"\nLast Year Assessed Value:\t{LastYearAssessedValue.ToString("c")}"+
                 $"\nCurrent Assessed Value:\t{CurrentYearAssessedValue().ToString("c")}"+
-                $"\nExemption:\t{Exemption.ToString("c")}"+
+                $"\nExemption:\t{exemption.ToString("c")}"+
                 $"\nTaxable Value:\t{TaxableValue().ToString("c")}"+
-                $"\nMillage Rate (per $1000):\t{MillageRate.ToString("c")}+" +
+                $"\nMillage Rate (per $1000):\t{millage_rate.ToString("c")}+" +
                 $"\nTaxes Due:\t{TaxesDue().ToString("c")}";
         }
 
