ec7a13381e833fc036c52f533888e25a501dff3e
Bartosz Dziuba <gothosfolly@protonmail.com>
2020-11-24 09:48:16 +0100

    ArrayList intro

---
+++ Section9/ListTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+
+namespace Section9
+{
+    [TestClass]
+    public class ListTest
+    {
+        [TestMethod]
+        public void Test_Crreating_Lists()
+        {
+            ArrayList scores = new ArrayList();
+            FillList(scores);
+            int sum = TotalScores(scores);
+            Console.WriteLine(sum);
+        }
+
+        public void FillList(ArrayList scores)
+        {
+            for(int i = 1; i<=20; i++)
+            {
+                scores.Add(i);
+            }
+        }
+
+        public int TotalScores(ArrayList scores)
+        {
+            int sum = 0;
+            foreach(int score in scores)
+            {
+                sum += score;
+            }
+            return sum;
+        }
+    }
+}
