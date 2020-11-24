07302514e75fe8f1f484114b1282573a41ea76e2
Bartosz Dziuba <gothosfolly@protonmail.com>
2020-11-24 10:21:05 +0100

    Section 9 final exam

--- Section9/ListTest.cs
+++ Section9/ListTest.cs
@@ -16,6 +16,40 @@ namespace Section9
             Console.WriteLine(sum);
         }
 
+        [TestMethod]
+        public void ArrayList_Practice()
+        {
+            ArrayList list = new ArrayList();
+
+            Console.WriteLine("Adding some numbers:");
+            list.Add(45);
+            list.Add(78);
+            list.Add(33);
+            list.Add(56);
+            list.Add(12);
+            list.Add(23);
+            list.Add(9);
+
+            Console.WriteLine("Capacity: {0}", list.Capacity);
+            Console.WriteLine("Count: {0}", list.Count);
+
+            Console.Write("Content: ");
+            foreach (int i in list)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Sorted Content: ");
+            list.Sort();
+            foreach (int i in list)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+
         public void FillList(ArrayList scores)
         {
             for(int i = 1; i<=20; i++)
---
+++ Section9/Section9Exam.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Section9
+{
+    [TestClass]
+    public class Section9Exam
+    {
+        [TestMethod]
+        public void Exam_Test()
+        {
+            ArrayList dataList = new ArrayList();
+            dataList.Add("Someday");
+            dataList.Add(2);
+            dataList.Add("app");
+            dataList.Add(13.5);
+            dataList.Add("red");
+            dataList.Add(8);
+            dataList.Add(99);
+            dataList.Add("6");
+            dataList.Add("Meatball soup");
+            dataList.Add(18M);
+            dataList.Add("pieces");
+            dataList.Add("14");
+
+            double total = SumArrayList(PruneArrayList(dataList));
+
+            Console.WriteLine(total);
+            Assert.AreEqual(160.5, total);
+        }
+
+        public ArrayList PruneArrayList(ArrayList dataList)
+        {
+            double inValue;
+            ArrayList cleanList = new ArrayList();
+
+            for (int i=0; i<dataList.Count; i++)
+            {
+                if (double.TryParse(Convert.ToString(dataList[i]), out inValue) == true)
+                {
+                    cleanList.Add(inValue);
+                }
+            }
+            return cleanList;
+        }
+
+        public double SumArrayList(ArrayList dataList)
+        {
+            double sum = 0.0;
+            foreach (double item in dataList)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+    }
+}
