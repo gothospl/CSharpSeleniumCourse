bdcd6d8265cf9e9da0ed27d685449568d0722aa2
Bartosz Dziuba <gothosfolly@protonmail.com>
2020-11-25 12:23:11 +0100

    Error Handling Exam

---
+++ Section12/ExamTester.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Section12
+{
+    //Create a try/catch block in each test that calls the method and catches the
+    //specific exception tat is produced. Each method should be handled so that if an
+    //exception occurs, the test will still pass
+    //write out the error message taht occurs so you can view it in the output
+
+    [TestClass]
+    public class ExamTester
+    {
+        [TestMethod]
+        public void Test_Weekly_Wages()
+        {
+            try
+            {
+                ExamHelper.WeeklyWages();
+            }
+            catch (IndexOutOfRangeException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+
+        [TestMethod]
+        public void Test_Calc_Result()
+        {
+            try
+            {
+                ExamHelper.CalculateResult();
+            }
+            catch (FormatException exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+
+        [TestMethod]
+        public void Test_Number_Check()
+        {
+            try
+            {
+                ExamHelper.NumberCheck();
+            }
+            catch (DivideByZeroException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+}
---
+++ Section13/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Section13")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Section13")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("93196fbf-a26b-4be0-b146-4a8a23328d4b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
